85c8a87e1ac97f81d6af87bbe0af2396a0690368
Leonardo Uieda <leo@uieda.com>
2024-03-11 15:25:17 -0300

    Format the tests

--- magali/tests/test_io.py
+++ magali/tests/test_io.py
@@ -1,6 +1,7 @@
 """
 Test the IO functions
 """
+
 import pooch
 
 from .._io import read_qdm_harvard
@@ -8,6 +9,9 @@ from .._io import read_qdm_harvard
 
 def test_read_qdm_harvard():
     "Try loading a sample dataset"
-    path = pooch.retrieve("doi:10.6084/m9.figshare.22965200.v1/Bz_uc0.mat", known_hash="md5:268bd3af5e350188d239ff8bd0a88227")
+    path = pooch.retrieve(
+        "doi:10.6084/m9.figshare.22965200.v1/Bz_uc0.mat",
+        known_hash="md5:268bd3af5e350188d239ff8bd0a88227",
+    )
     data = read_qdm_harvard(path)
     assert data.bz.size == 576_000
