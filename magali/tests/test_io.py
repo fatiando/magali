1a7739df6d0da08f63f7cbd9ab58d01713611894
Leonardo Uieda <leo@uieda.com>
2024-03-11 15:32:31 -0300

    Update the testing Action workflow (#1)
    Python versions, Dependente version, and the CodeCov action. Downgrade
    latest Python to 3.12 because Verde fails to import pkg_resources and 
    crashes.

---
+++ magali/tests/test_io.py
@@ -0,0 +1,17 @@
+"""
+Test the IO functions
+"""
+
+import pooch
+
+from .._io import read_qdm_harvard
+
+
+def test_read_qdm_harvard():
+    "Try loading a sample dataset"
+    path = pooch.retrieve(
+        "doi:10.6084/m9.figshare.22965200.v1/Bz_uc0.mat",
+        known_hash="md5:268bd3af5e350188d239ff8bd0a88227",
+    )
+    data = read_qdm_harvard(path)
+    assert data.bz.size == 576_000
