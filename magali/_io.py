e2128a98285de3f30f7b97f08b4df652ee8807ce
Leonardo Uieda <leo@uieda.com>
2024-03-12 14:06:11 -0300

    Fill out the docstring for the read_qdm_harvard
    The docstring only had the title. Add the missing sections.

--- magali/_io.py
+++ magali/_io.py
@@ -18,20 +18,36 @@ from ._constants import METER_TO_MICROMETER, TESLA_TO_NANOTESLA
 def read_qdm_harvard(path):
     """
     Load QDM microscopy data in the Harvard group format.
+
+    This is the file type used by Roger Fu's group to distribute QDM data. It's
+    a Matlab binary file that has the data and some information about grid
+    spacing.
+
+    Parameters
+    ----------
+    path : str or pathlib.Path
+
+    Returns
+    -------
+    data : xarray.Dataset
+        The magnetic field data as a regular grid with coordinates. The
+        coordinates are in µm and magnetic field in nT.
     """
     contents = scipy.io.loadmat(path)
     # For some reason, the spacing is returned as an array with a single
     # value. That messes up operations below so get the only element out.
     spacing = contents["step"].ravel()[0] * METER_TO_MICROMETER
     bz = contents["Bz"] * TESLA_TO_NANOTESLA
+    data_names = ["bz"]
+    shape = bz.shape
     sensor_sample_distance = contents["h"] * METER_TO_MICROMETER
-    x = np.arange(bz.shape[1]) * spacing
-    y = np.arange(bz.shape[0]) * spacing
-    z = np.full(bz.shape, sensor_sample_distance)
+    x = np.arange(shape[1]) * spacing
+    y = np.arange(shape[0]) * spacing
+    z = np.full(shape, sensor_sample_distance)
     data = vd.make_xarray_grid(
         (x, y, z),
         bz,
-        data_names=["bz"],
+        data_names=data_names,
         dims=("y", "x"),
         extra_coords_names="z",
     )
