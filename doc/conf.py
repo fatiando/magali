e04f2b699cd0f685a3926e477e618c4f883b3b1a
Leonardo Uieda <leo@uieda.com>
2024-03-12 08:49:35 -0300

    Run the formatters

--- doc/conf.py
+++ doc/conf.py
@@ -8,7 +8,9 @@ import magali
 # Project information
 # -----------------------------------------------------------------------------
 project = "magali"
-copyright = f"{datetime.date.today().year}, The {project.title()} developers"  # noqa: A001
+copyright = (
+    f"{datetime.date.today().year}, The {project.title()} developers"  # noqa: A001
+)
 if len(magali.__version__.split(".")) > 3:
     version = "dev"
 else:
