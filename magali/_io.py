4efddad8c5868e255b2c7103eb7edb4a3cd6ccb2
YagoMCastro <yagomcastro1@gmail.com>
2024-03-18 09:00:53 -0300

    Add grid_qdm_data funciton

--- magali/_io.py
+++ magali/_io.py
@@ -36,18 +36,8 @@ def read_qdm_harvard(path):
     """
     contents = scipy.io.loadmat(path)
     coordinates, data_names, bz = set_qdm_data_grid(contents)
-    data = vd.make_xarray_grid(
-        (x, y, z),
-        bz,
-        data_names=data_names,
-        dims=("y", "x"),
-        extra_coords_names="z",
-    )
-    data.x.attrs = {"units": "µm"}
-    data.y.attrs = {"units": "µm"}
-    data.z.attrs = {"long_name": "sensor sample distance", "units": "µm"}
-    data.bz.attrs = {"long_name": "vertical magnetic field", "units": "nT"}
-    data.attrs = {"file_name": str(path)}
+    data = grid_qdm_data(coordinates, data_names, bz, path)
+
     return data
 
 def set_qdm_data_grid(contents):
@@ -85,3 +75,48 @@ def set_qdm_data_grid(contents):
     z = np.full(shape, sensor_sample_distance)
     
     return (x, y, z), data_names, bz
+
+def grid_qdm_data(coordinates, data_names, bz, path):
+    """
+    Load QDM microscopy data in the Harvard group format.
+
+    This is the file type used by Roger Fu's group to distribute QDM data. It's
+    a Matlab binary file that has the data and some information about grid
+    spacing.
+
+    Parameters
+    ----------
+    coordinates: tuple of arrays
+        Arrays with coordinates of each point in the grid. Each array must contain values for a 
+        dimension in the order: easting, northing, vertical, etc. All arrays must be 2d and need to 
+        have the same shape. These coordinates can be generated through verde.grid_coordinates.
+    
+    data_names : str or list
+        The name(s) of the data variables in the output grid. Ignored if data is None.
+
+    path : str or pathlib.Path
+        Path to the input Matlab binary file.
+    
+    bz : array
+        The Bz component in nT.
+
+    Returns
+    -------
+    qdm_data : xarray.Dataset
+        The magnetic field data as a regular grid with coordinates. The
+        coordinates are in µm and magnetic field in nT.
+    """
+    qdm_data = vd.make_xarray_grid(
+        coordinates,
+        bz,
+        data_names=data_names,
+        dims=("y", "x"),
+        extra_coords_names="z",
+    )
+    qdm_data.x.attrs = {"units": "µm"}
+    qdm_data.y.attrs = {"units": "µm"}
+    qdm_data.z.attrs = {"long_name": "sensor sample distance", "units": "µm"}
+    qdm_data.bz.attrs = {"long_name": "vertical magnetic field", "units": "nT"}
+    qdm_data.attrs = {"file_name": str(path)}
+    
+    return qdm_data
