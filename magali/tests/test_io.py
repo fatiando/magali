15704736e443dbb929865440af77abd46014d8ae
Yago Moreira Castro <65922077+YagoMCastro@users.noreply.github.com>
2025-02-26 17:52:05 -0300

    Write tests for read_qdm_harvard (#51)
    Add tests to check attributes for the data loaded by
    `magali.read_dqm_harvard` function. The attributes checked are:
    - Coordinates size (`bz.x.size`, `bz.z.size`, `bz.z.size`)
    - Coordinates units (`bz.x.units`, `bz.z.units`, `bz.z.units`)
    - `bz.units`
    - `bz.size`
    - `bz.long_name`
    - `bz.z.long_name`
    
    This PR also adds tests if bz and all of it's attributes are DataArrays.
    
    **Relevant issues/PRs:**
    Solves #49

--- magali/tests/test_io.py
+++ magali/tests/test_io.py
@@ -9,6 +9,7 @@ Test the IO functions
 """
 
 import ensaio
+import xarray as xr
 
 from .._input_output import read_qdm_harvard
 
@@ -17,4 +18,26 @@ def test_read_qdm_harvard():
     "Try loading a sample dataset"
     fname = ensaio.fetch_morroco_speleothem_qdm(version=1, file_format="matlab")
     bz = read_qdm_harvard(fname)
+
+    # Test units
+    assert bz.x.units == "µm"
+    assert bz.y.units == "µm"
+    assert bz.z.units == "µm"
+    assert bz.units == "nT"
+
+    # Test array sizes
     assert bz.size == 576_000
+    assert bz.x.size == 960
+    assert bz.y.size == 600
+    assert bz.z.size == 576000
+    assert bz.size == 576000
+
+    # Test data name
+    assert bz.long_name == "vertical magnetic field"
+    assert bz.z.long_name == "sensor sample distance"
+
+    # Test if bz is a DataArray
+    assert isinstance(bz, xr.DataArray)
+    assert isinstance(bz.x, xr.DataArray)
+    assert isinstance(bz.y, xr.DataArray)
+    assert isinstance(bz.z, xr.DataArray)
