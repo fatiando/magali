4c639ffeb59553d27a3ccda82de653fa6c848779
Yago Moreira Castro <yagomcastro1@gmail.com>
2025-03-19 17:20:08 -0300

    calculate bz on tests

--- magali/tests/test_synthetic.py
+++ magali/tests/test_synthetic.py
@@ -10,10 +10,11 @@ Test the _synthetic functions
 
 import harmonica as hm
 import numpy as np
+import verde as vd
 import xarray as xr
 
 from .._stats import variance
-from .._synthetic import dipole_bz_grid, random_directions
+from .._synthetic import dipole_bz, dipole_bz_grid, random_directions
 
 
 def test_random_directions():
@@ -56,6 +57,44 @@ def test_random_directions():
     )
     std_dev = np.sqrt(directions_variance)
     np.testing.assert_allclose(true_dispersion_angle, std_dev, rtol=1e-3)
+
+
+def test_dipole_bz():
+    sensor_sample_distance = 5.0  # µm
+
+    coordinates = vd.grid_coordinates(
+        region=[0, 2000, 0, 2000],  # µm
+        spacing=2,  # µm
+        extra_coords=sensor_sample_distance,
+    )
+
+    true_inclination = 30
+    true_declination = 40
+    true_dispersion_angle = 5
+
+    size = 10
+
+    directions_inclination, directions_declination = random_directions(
+        true_inclination,
+        true_declination,
+        true_dispersion_angle,
+        size=size,
+        random_state=5,
+    )
+
+    amplitude = abs(np.random.normal(0, 100, size)) * 1.0e-14
+
+    dipole_moments = hm.magnetic_angles_to_vec(
+        directions_inclination, directions_declination, amplitude
+    )
+
+    dipole_coordinates = (
+        np.random.randint(30, 1970, size),  # µm
+        np.random.randint(30, 1970, size),  # µm
+        np.random.randint(-20, -1, size),  # µm
+    )
+
+    bz = dipole_bz(coordinates, dipole_coordinates, dipole_moments)
 
 
 def test_dipole_bz_grid():
