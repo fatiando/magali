9b28cd4e0a7ca6f4d5dc9df0759e11c750b7384c
Yago M. Castro <yagomcastro1@gmail.com>
2025-03-31 16:20:20 -0300

    Wrtie tests for the detect_anomalies function

--- magali/__init__.py
+++ magali/__init__.py
@@ -4,6 +4,7 @@
 #
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
+from ._detection import detect_anomalies
 from ._input_output import read_qdm_harvard
 from ._synthetic import dipole_bz, dipole_bz_grid, random_directions
 from ._utils import (
@@ -11,6 +12,4 @@ from ._utils import (
     total_gradient_amplitude,
     total_gradient_amplitude_grid,
 )
-from ._detection import detect_anomalies
-
 from ._version import __version__
