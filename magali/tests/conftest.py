23eb5ec3aab1c9311aafc926d51ad966ad586d3b
Yago Moreira Castro <yagomcastro1@gmail.com>
2025-04-03 11:21:43 -0300

    Write a model simmilar to souza-junior's

--- magali/tests/conftest.py
+++ magali/tests/conftest.py
@@ -12,7 +12,7 @@ import harmonica as hm
 import numpy as np
 import pytest
 
-from .._synthetic import dipole_bz_grid
+from .._synthetic import dipole_bz_grid, random_directions
 
 
 @pytest.fixture
@@ -38,4 +38,46 @@ def simple_model():
 
     return dipole_bz_grid(
         region, spacing, sensor_sample_distance, dipole_coordinates, dipole_moments
+    )
+
+
+
+@pytest.fixture
+def souza_junior_model():
+    """
+    A model simmilar to the one used by Souza-Junior
+    """
+    sensor_sample_distance = 5.0  # µm
+    region = [0, 2000, 0, 2000]  # µm
+    spacing = 2  # µm
+
+    true_inclination = 30
+    true_declination = 40
+    true_dispersion_angle = 5
+
+    size = 100
+
+    directions_inclination, directions_declination = random_directions(
+    true_inclination,
+    true_declination,
+    true_dispersion_angle,
+    size=size,
+    random_state=5,
+    )
+
+    dipoles_amplitude = abs(np.random.normal(0, 100, size)) * 1.0e-14
+
+    dipole_coordinates = (
+    np.concatenate([np.random.randint(30, 1970, size), [1250, 1300, 500]]),  # µm
+    np.concatenate([np.random.randint(30, 1970, size), [500, 1750, 1000]]),  # µm
+    np.concatenate([np.random.randint(-20, -1, size), [-15, -15, -30]]),  # µm
+    )
+    dipole_moments = hm.magnetic_angles_to_vec(
+    inclination=np.concatenate([directions_inclination, [10, -10, -5]]),
+    declination=np.concatenate([directions_declination, [10, 170, 190]]),
+    intensity=np.concatenate([dipoles_amplitude, [5e-11, 5e-11, 5e-11]]),
+    )
+
+    return dipole_bz_grid(
+    region, spacing, sensor_sample_distance, dipole_coordinates, dipole_moments
     )
