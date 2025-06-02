d529bf82eab03edfd9176c0c37c95ac3e4e8650e
Yago Moreira Castro <yagomcastro1@gmail.com>
2025-06-02 08:31:43 -0300

    Add tmp file and NonLinearMagneticMomentBz back

--- src/magali/_inversion.py
+++ src/magali/_inversion.py
@@ -11,7 +11,10 @@ Classes for inversions.
 import choclo
 import numba
 import numpy as np
+import verde as vd
+from scipy.optimize import minimize
 
+from ._synthetic import dipole_bz
 from ._units import coordinates_micrometer_to_meter, tesla_to_nanotesla
 from ._validation import check_fit_input
 
@@ -121,3 +124,168 @@ def _jacobian(x, y, z, xc, yc, zc, result):
 # Compile the Jacobian calculation. Doesn't use this as a decorator so that we
 # can test the pure Python function and get coverage information about it.
 jacobian_jit = numba.jit(_jacobian, nopython=True, parallel=True)
+
+
+class NonLinearMagneticMomentBz:
+    r"""
+    Estimate dipole location and moment from Bz data using nonlinear optimization.
+
+    Fits a magnetic point dipole model to Bz measurements by simultaneously estimating
+    the dipole position and moment vector that minimize the squared misfit between
+    observed and predicted fields.
+
+    Parameters
+    ----------
+    observed_data : xarray.Dataset
+        Gridded dataset containing the observed Bz field.
+    initial_position : array-like
+        Initial guess for the dipole location (x, y, z), in meters.
+    initial_moment : array-like
+        Initial guess for the dipole moment vector (mx, my, mz), in Am².
+    background_field : array-like or None, optional
+        Optional background field to subtract from the observed Bz values.
+    optimization_method : str, optional
+        Optimization method to use (default is "Nelder-Mead"). Any method accepted
+        by `scipy.optimize.minimize` can be used.
+
+    Attributes
+    ----------
+    optimized_position_ : ndarray
+        Estimated position of the magnetic dipole (x, y, z), in meters.
+    optimized_moment_ : ndarray
+        Estimated magnetic moment vector (mx, my, mz), in Am².
+    result_ : OptimizeResult
+        Full result object returned by `scipy.optimize.minimize`.
+
+    Methods
+    -------
+    fit()
+        Run non-linear optimization to simultaneously estimate position and moment.
+    """
+
+    def __init__(
+        self,
+        observed_data,
+        initial_position,
+        initial_moment,
+        background_field=None,
+        optimization_method="Nelder-Mead",
+    ):
+        self.observed_data = observed_data
+        self.background_field = (
+            np.ravel(np.asarray(background_field))
+            if background_field is not None
+            else None
+        )
+        self.initial_position = np.asarray(initial_position)
+        self.initial_moment = np.asarray(initial_moment)
+        self.optimization_method = optimization_method
+        self.optimized_position_ = None
+        self.optimized_moment_ = None
+        self.result_ = None
+
+    def _normalize(self):
+        """
+        Normalize parameters using the magnitude of the initial estimates.
+
+        Returns
+        -------
+        x0_norm : array
+            Normalized position vector.
+        m0_norm : array
+            Normalized moment vector.
+        """
+        self._x_scale = np.maximum(
+            np.abs(self.initial_position), 1e-12
+        )  # 1e-12 is used to avoid division by zero
+        self._m_scale = np.linalg.norm(self.initial_moment) or 1e-12
+
+        x0_norm = self.initial_position / self._x_scale
+        m0_norm = self.initial_moment / self._m_scale
+        return x0_norm, m0_norm
+
+    def _denormalize(self, x_norm, m_norm):
+        """
+        Restore original scale from normalized parameters.
+
+        Parameters
+        ----------
+        x_norm : array
+            Normalized position vector.
+        m_norm : array
+            Normalized magnetic moment vector.
+
+        Returns
+        -------
+        x : array
+            Denormalized position vector.
+        m : array
+            Denormalized moment vector.
+        """
+        x = x_norm * self._x_scale
+        m = m_norm * self._m_scale
+        return x, m
+
+    def _misfit(self, params):
+        """
+        Compute L2 norm misfit between base-corrected observed and predicted data.
+
+        Misfit function: ||(d0 - b) - d(x, m)||^2
+
+        Parameters
+        ----------
+        params : ndarray
+            Flattened parameter vector
+            [x_norm, y_norm, z_norm, mx_norm, my_norm, mz_norm].
+
+        Returns
+        -------
+        misfit : float
+            Sum of squared residuals between observed and predicted Bz values.
+        """
+        x = params[:3]
+        m = params[3:]
+        x, m = self._denormalize(x, m)
+        data = vd.grid_to_table(self.observed_data)
+        coordinates = (
+            data.x,
+            data.y,
+            data.z,
+        )
+        model = dipole_bz(coordinates, x, m)
+        if self.background_field is None:
+            corrected_data = data.bz
+        else:
+            corrected_data = data.bz - self.background_field
+        residuals = corrected_data - model
+        return np.sum(residuals**2)
+
+    def fit(self):
+        """
+        Estimate the dipole position and moment via nonlinear least-squares optimization.
+
+        Uses the selected optimization method to minimize the misfit between the
+        observed and predicted Bz values.
+
+        Returns
+        -------
+        self : NonLinearMagneticMomentBz
+            The instance, updated with the fitted `optimized_position_` and `optimized_moment_`.
+        """
+        x0_norm, m0_norm = self._normalize()
+        initial_params = np.hstack((x0_norm, m0_norm))
+
+        result = minimize(
+            fun=self._misfit,
+            x0=initial_params,
+            method=self.optimization_method,
+            options={"maxiter": 1000, "disp": True},
+        )
+
+        self.result_ = result
+        x_norm_opt = result.x[:3]
+        m_norm_opt = result.x[3:]
+        self.optimized_position_, self.optimized_moment_ = self._denormalize(
+            x_norm_opt, m_norm_opt
+        )
+        return self
